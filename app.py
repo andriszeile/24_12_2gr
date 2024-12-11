8e7cfa147df5f2fda79925a38d68d10710524a16
andriszeile <71541542+andriszeile@users.noreply.github.com>
2024-12-11 13:00:03 +0200

    111

--- app.py
+++ app.py
@@ -25,13 +25,13 @@ def index():
     return render_template("index.html")
 
 #API, lai iegūtu visus ierakstus
-@app.route("api/data", methods=["GET"])
+@app.route("/api/data", methods=["GET"])
 def get_data():
     data = load_data()
     return jsonify(data)
 
 #API, lai pievienotu datus
-@app.route("api/data", methods=["POST"])
+@app.route("/api/data", methods=["POST"])
 def add_data():
     request_data = request.json
     date = request_data.get("date")
